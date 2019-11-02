3dad43b89645cf1e0335764df3915500d85d58f9
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2019-11-03 02:34:41 +0500

    added client implementation

--- src/HelloGrpc.Client/Program.cs
+++ src/HelloGrpc.Client/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using HelloGrpc.Server;
 
 namespace HelloGrpc.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calling SayHello Endpoint with name: 'World'");
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var helloReply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+            Console.WriteLine($"Response from server, '{helloReply.Message}'");
         }
     }
 }
